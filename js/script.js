fe002c27c37386d7ebd467e3e5fcc82dd16c567c
Ninna994 <inanikolina@gmail.com>
2021-09-27 21:48:37 +0200

    100 days of CSS. Days 1,2,4,6 finished

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('.bar-container').on('click', function () {
+    $('.bar-container').toggleClass('active');
+    $('.bar-1').removeClass('no-animation');
+    $('.bar-2').removeClass('no-animation');
+    $('.bar-3').removeClass('no-animation');
+});
