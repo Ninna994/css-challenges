c52b0956b1704bb92b72e03ffbb4ad4dbae4e27c
Ninna994 <inanikolina@gmail.com>
2021-09-28 20:30:22 +0200

    Day 7 finished

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ $('.bar-container').on('click', function () {
     $('.bar-2').removeClass('no-animation');
     $('.bar-3').removeClass('no-animation');
 });
+
+// challenge 7
+
+$('.bar').on('click', function () {
+    $('.notifications').toggleClass('notification-move');
+    // console.log('evo me');
+    $('.menu').toggleClass('menu-active');
+
+})
+
+$('.search-icon').on('click', function () {
+    $('.search-input').toggleClass('show-search');
+})
